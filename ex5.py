79ef3d2c69d8cf677970218d1cc9afb886eb26a2
Paloma Santos <palomattss@gmail.com>
2022-09-05 22:39:18 -0300

    Exercício 5

--- ex5.py
+++ ex5.py
@@ -1,12 +1,12 @@
-# Escreva uma classe “Quadrado”, crie dois métodos que retornem a
-# área do quadrado e o perímetro, não crie a instância nesse exercício.
+# Escreva uma classe “Quadrado”, crie dois métodos que retornem a área do quadrado e o perímetro, não crie a instância nesse exercício.
 
 class Quadrado:
     def __init__(self,lado):
-    self.lado = lado
+        self.lado = lado
     
     def area(self):
         return lado*lado
     
     def perimetro(self):
         return lado*4
+
